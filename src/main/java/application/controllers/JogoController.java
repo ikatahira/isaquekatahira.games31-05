6d9b9f1adb41ea5b3e7594a5f23d7a2614d6e93b
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-05-31 19:54:56 +0000

    finalizando os controladores

--- src/main/java/application/controllers/JogoController.java
+++ src/main/java/application/controllers/JogoController.java
@@ -1,6 +1,8 @@
 package application.controllers;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,13 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.yaml.snakeyaml.util.PlatformFeatureDetector;
 
 import application.models.Jogo;
 import application.models.Plataforma;
 import application.repositories.GeneroRepository;
 import application.repositories.JogoRepository;
 import application.repositories.PlataformaRepository;
+
+
 @Controller
 @RequestMapping("/jogos")
 public class JogoController {
@@ -65,22 +68,33 @@ public class JogoController {
 
         if(!jogo.isPresent())
             return "redirect:/jogos/list";
-        model.addAttribute("jogo", jogo.get());
+
+        model.addAttribute("jogos", jogo.get());
+        model.addAttribute("generos", generosRepo.findAll());
+        model.addAttribute("plafaformas", plataformasRepo.findAll());
         return "/jogos/update.jsp";
-    
-    
+            
     
     }
 
     @RequestMapping(value = "update", method = RequestMethod.POST)
-    public String saveUpdate(@RequestParam("titulo") String titulo, @RequestParam("id") int id) {
+    public String saveUpdate(@RequestParam("titulo") String titulo, @RequestParam("id") int id,
+    @RequestParam("genero") int generoId, @RequestParam(name="plataformas", required =false)int[] plafaformas) {
         Optional<Jogo> jogo = jogosRepo.findById(id);
         if(!jogo.isPresent())
             return "redirect:/jogos/list";
         jogo.get().setTitulo(titulo);
-
+        jogo.get().setGenero(generosRepo.findById(generoId).get());
+        Set<Plataforma> updatePlataforma = new HashSet<>();
+        
+        if(plataformas!=null)
+            for(int p: plataformas){
+                Optional<Plataforma> plataforma = plataformasRepo.findById(p);
+                if(plataforma.isPresent())
+                    updatePlataforma.add(plataforma.get());
+            }
+        jogo.get().setPlataformas(updatePlataforma);
         jogosRepo.save(jogo.get());
-
         return "redirect:/jogos/list";
     }
 
